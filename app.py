0934da7a715a8a0047f895b7e5fe2023db90e38e
M VR <taiogao@gmail.com>
2026-04-27 18:29:14 +0200

    Cambiado port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 
@@ -52,4 +53,5 @@ def album(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
